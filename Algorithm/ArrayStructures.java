d5036299497a0ee3f21d15400b305607cd6ff640
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-05-03 00:21:39 +0700

    Update ArrayStructures.java

--- Algorithm/ArrayStructures.java
+++ Algorithm/ArrayStructures.java
@@ -20,7 +20,65 @@ public class ArrayStructures {
 
             System.out.println("----------");
         }
-        
+    }
+
+    public int getValueAtIndex(int index) {
+        if(index < arraySize) {
+            return theArray[index];
+        }
+        return 0;
+    }
+
+    public boolean doesArrayContainValue(int searchValue) {
+        boolean valueInArray = false;
+        for(int i = 0; i < arraySize; i++) {
+            if(theArray[i] == searchValue) {
+                valueInArray = true;
+            }
+        }
+        return valueInArray;
+    }
+
+    //Delete an index
+    public void deleteIndex(int index) {
+        if(index < arraySize) {
+            for(int i = index; i < (arraySize - 1); i++) {
+                theArray[i] = theArray[i + 1];
+            }
+            arraySize--;
+        }
+    }
+
+    //Add a value to the bottom
+    public void insertValue(int value) {
+        if(arraySize < 50) {
+            theArray[arraySize] = value;
+            arraySize++;
+        }
+    }
+
+    //Linear Search
+    public String linearSearchForValue(int value) {
+        boolean valueInArray = false;
+        String indexWithValue = "";
+        System.out.print("The value was found in the following: ");
+
+        for(int i = 0; i < arraySize; i++) {
+            if(theArray[i] == value) {
+                valueInArray = true;
+                System.out.print(i + " ");
+                indexWithValue += i + " ";
+            }
+        }
+
+        if (!valueInArray) {
+            indexWithValue = "None";
+            System.out.print(indexWithValue);
+        }
+
+        System.out.println();
+        return indexWithValue;
+
     }
 
     public static void main(String[] args) {
@@ -29,5 +87,17 @@ public class ArrayStructures {
         newArray.generateRandomArray();
 
         newArray.printArray();
+
+        System.out.println(newArray.getValueAtIndex(5));
+
+        System.out.println(newArray.doesArrayContainValue(18));
+
+        newArray.deleteIndex(4);
+
+        newArray.insertValue(78);
+
+        newArray.printArray();
+
+        newArray.linearSearchForValue(78);
     }
 }
