4c0573576c3ce2e41351fba38e12d7eae37402c4
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-05-02 23:56:31 +0700

    Add files via upload

---
+++ Algorithm/ArrayStructures.java
@@ -0,0 +1,33 @@
+
+public class ArrayStructures {
+
+    private int[] theArray = new int[50];
+
+    private int arraySize = 10;
+
+    public void generateRandomArray() {
+        for(int i = 0; i < arraySize; i++){
+            theArray[i] = (int)(Math.random() * 10) + 10;
+        }
+    }
+
+    public void printArray() {
+        System.out.println("----------");
+        for(int i = 0; i < arraySize; i++) {
+
+            System.out.print("| " + i + " | ");
+            System.out.println(theArray[i] + " |");
+
+            System.out.println("----------");
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        ArrayStructures newArray = new ArrayStructures();
+
+        newArray.generateRandomArray();
+
+        newArray.printArray();
+    }
+}
